56f5c8ae4040d96267a1e1b69852bf4ca5d55825
panxiaoan <xiaoan.pan@qq.com>
2018-08-15 14:11:34 +0800

    简单更新

--- zuul-server/src/main/java/com/panxiaoan/springcloud/sample/RunZuulServerApp.java
+++ zuul-server/src/main/java/com/panxiaoan/springcloud/sample/RunZuulServerApp.java
@@ -3,17 +3,19 @@ package com.panxiaoan.springcloud.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * 会员服务
+ * Zull Server
  *  
  * @author <a href="mailto:xiaoan.pan@qq.com">潘小安</a> 
  * @since 2018-08-15 01:00 
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class RunZullServerApp {
+@EnableZuulProxy
+public class RunZuulServerApp {
 	public static void main(String[] args) {
-		SpringApplication.run(RunZullServerApp.class, args);
+		SpringApplication.run(RunZuulServerApp.class, args);
 	}
 }
